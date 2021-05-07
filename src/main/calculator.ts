516cd5e02d76283b8c0fd56c9f35c30dcec1f729
James Cooke <james.cooke@cazoo.co.uk>
2021-05-07 11:13:34 +0100

    Adding test for 4 numbers

--- src/main/calculator.ts
+++ src/main/calculator.ts
@@ -4,18 +4,14 @@ export default class StringCalculator {
       return 0;
     }
 
-    if (input === "1,1,1") {
-      return 3;
-    }
-
-    if (input === "2,2,2") {
-      return 6;
-    }
-
     const stringNumbers = input.split(",");
 
     if (stringNumbers.length === 1) return Number(stringNumbers[0]);
 
-    return Number(stringNumbers[0]) + Number(stringNumbers[1]);
+    if (stringNumbers.length === 2) {
+        return Number(stringNumbers[0]) + Number(stringNumbers[1]);
+    }
+
+    return Number(stringNumbers[0]) + Number(stringNumbers[1]) + Number(stringNumbers[2]);
   }
 }
--- src/test/calculator.test.ts
+++ src/test/calculator.test.ts
@@ -10,7 +10,7 @@ describe("String calculator", () => {
     ["1", 1],
     ["2", 2],
     ["3", 3],
-  ])("given %i should return %i", (stringInput, expected) => {
+  ])("given %s should return %i", (stringInput, expected) => {
     const stringCalculator: StringCalculator = new StringCalculator();
     expect(stringCalculator.add(stringInput)).toBe(expected);
   });
@@ -19,23 +19,24 @@ describe("String calculator", () => {
     ["1,1", 2],
     ["5,11", 16],
     ["0,8", 8],
-  ])("given %i should return %i", (stringInput, expected) => {
+  ])("given %s should return %i", (stringInput, expected) => {
     const stringCalculator: StringCalculator = new StringCalculator();
     expect(stringCalculator.add(stringInput)).toBe(expected);
   });
 
-  it("given '1,1,1' should return 3", () => {
+  it.each([
+    ["1,1,1", 3],
+    ["2,2,2", 6],
+    ["2,3,4", 9],
+  ])("given %s should return %i", (stringInput, expected) => {
     const stringCalculator: StringCalculator = new StringCalculator();
-    expect(stringCalculator.add("1,1,1")).toBe(3);
+    expect(stringCalculator.add(stringInput)).toBe(expected);
   });
 
-  it("given '2,2,2' should return 6", () => {
+  it.each([
+    ["1,1,1,1", 4],
+  ])("given %s should return %i", (stringInput, expected) => {
     const stringCalculator: StringCalculator = new StringCalculator();
-    expect(stringCalculator.add("2,2,2")).toBe(6);
-  });
-
-  it("given '2,3,4' should return 9", () => {
-    const stringCalculator: StringCalculator = new StringCalculator();
-    expect(stringCalculator.add("2,3,4")).toBe(9);
+    expect(stringCalculator.add(stringInput)).toBe(expected);
   });
 });
